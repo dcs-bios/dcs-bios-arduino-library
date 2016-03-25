d25c5c51cf3597565f132dc2375fae8c9db51f47
Jan Böcker <jan.boecker@jboecker.de>
2016-03-25 21:01:22 +0100

    fix MasterCaution sketch

--- examples/MasterCaution/MasterCaution.ino
+++ examples/MasterCaution/MasterCaution.ino
@@ -4,8 +4,10 @@
 #define DCSBIOS_IRQ_SERIAL
 #include "DcsBios.h"
 
-DcsBios::Switch2Pos lampTestBtn("LAMP_TEST_BTN", 10);
-DcsBios::LED nmspIlsLed(0x1112, 0x0020, 13);
+/* Declare a Master Caution Reset button on pin 10 */
+DcsBios::Switch2Pos masterCautionBtn("UFC_MASTER_CAUTION", 10);
+/* Make the LED connected to pin 13 into a Master Caution Light */
+DcsBios::LED mcLed(0x1012, 0x0800, 13);
 
 void setup() {
   DcsBios::setup();
