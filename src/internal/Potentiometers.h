b8faa8e5647c9222aa67b543bb3e19947b55a3db
Jan Böcker <jan.boecker@jboecker.de>
2017-04-27 02:12:05 +0200

    add exponential weighted moving average filter to DcsBios::Potentiometer class

--- src/internal/Potentiometers.h
+++ src/internal/Potentiometers.h
@@ -1,16 +1,29 @@
 #ifndef __DCSBIOS_POTS_H
 #define __DCSBIOS_POTS_H
 
+#include <math.h>
 #include "Arduino.h"
 #include "PollingInput.h"
 
 namespace DcsBios {
 
-	class Potentiometer : PollingInput {
+	template <unsigned int pollInterval = 5, unsigned int hysteresis = 128, unsigned int ewma_divisor = 5>
+	class PotentiometerEWMA : PollingInput {
 		private:
 			void pollInput() {
+				unsigned char now = (unsigned char)millis();
+				if ((unsigned char)(now - lastPollTime_) < pollInterval) return;
+				lastPollTime_ = now;
+				
 				unsigned int state = map(analogRead(pin_), 0, 1023, 0, 65535);
-				if (state != lastState_) {
+				accumulator += ((float)state - accumulator) / (float)ewma_divisor;
+				state = (unsigned int)accumulator;
+				
+				if ((lastState_ > state && (lastState_ - state > hysteresis))
+				|| (state > lastState_) && (state - lastState_ > hysteresis)
+				|| (state > (65535 - hysteresis) && state > lastState_)
+				|| (state < hysteresis && state < lastState_)
+				) {
 					char buf[6];
 					utoa(state, buf, 10);
 					if (sendDcsBiosMessage(msg_, buf))
@@ -20,15 +33,21 @@ namespace DcsBios {
 			const char* msg_;
 			char pin_;
 			unsigned int lastState_;
+			float accumulator;
+			unsigned char lastPollTime_;
+			
 		public:
-			Potentiometer(const char* msg, char pin) {
+			PotentiometerEWMA(const char* msg, char pin) {
 				msg_ = msg;
 				pin_ = pin;
 				pinMode(pin_, INPUT);
-				lastState_ = map(analogRead(pin_), 0, 1023, 0, 65535);
+				lastState_ = (float)map(analogRead(pin_), 0, 1023, 0, 65535);
+				lastPollTime_ = (unsigned char)millis();
 			}
 	};
 
+	typedef PotentiometerEWMA<> Potentiometer;
+	
 }
 
 #endif
